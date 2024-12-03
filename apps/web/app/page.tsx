13f9135ec43609f5750b1e935ad573cb98ccd920
Abhishtt21 <100503970+Abhishtt21@users.noreply.github.com>
2024-12-03 23:00:34 +0530

    Project Pushed

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import { Landing } from "../components/Landing";
+import styles from "./page.module.css";
+
+export default function Page(): JSX.Element {
+  return (
+    <main>
+      <Landing />
+    </main>
+  );
+}
+
+export const dynamic = "force-dynamic"
